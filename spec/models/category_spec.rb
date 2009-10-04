6a44c9acc5f1d98970c751e3b362af534ccfcf2a
Martin Sarapik <martin@urgas.eu>
2009-10-04 18:46:47 +0300

    Added the required gems plus some model specs

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -9,6 +9,19 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe Category do
-  before(:each) do
+  before do
+    @category = new_category
+  end
+
+  describe 'name' do
+    it 'should not be valid if the name exceeds 20 characters' do
+      @category.name = 'a' * 30
+      @category.should_not be_valid
+    end
+
+    it 'should not be valid if the name is empty' do
+      @category.name = ''
+      @category.should_not be_valid
+    end
   end
 end
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -13,6 +13,35 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe Comment do
-  before(:each) do
+  before do
+    @comment = new_comment
+  end
+
+  it 'should be valid' do
+    @comment.should be_valid
+  end
+
+  describe 'author' do
+    it 'should not be valid if the author is more than 20 characters long' do
+      @comment.author = 'a' * 30
+      @comment.should_not be_valid
+    end
+
+    it 'should not be valid if the author is less than 3 characters long' do
+      @comment.author = 'ab'
+      @comment.should_not be_valid
+    end
+  end
+
+  describe 'content' do
+    it 'should not be valid if the content is less than 3 characters long' do
+      @comment.content = 'ab'
+      @comment.should_not be_valid
+    end
+
+    it 'should not be valid if the content is more than 500 characters long' do
+      @comment.content = 'a' * 510
+      @comment.should_not be_valid
+    end
   end
 end
