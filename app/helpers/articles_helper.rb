3b9ad80cb9b617223b9f8e5df73e3022cc9d39a7
Martin Sarapik <martin@urgas.eu>
2009-10-27 10:45:15 +0200

    A few minor tweaks

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,2 +1,5 @@
 module ArticlesHelper
+  def current_page?(options = {})
+    root_path == request.request_uri ? true : super
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 ActionController::Routing::Routes.draw do |map|
   map.resources :articles, :categories, :sessions
-  map.articles_by_category 'category/:category_name', :controller => 'articles', :action => 'by_category'
-  map.root :controller => 'articles'
 
   map.login 'login', :controller => 'sessions', :action => 'new'
   map.logout 'logout', :controller => 'sessions', :action => 'destroy'
+
+  map.articles_by_category 'category/:category_name', :controller => 'articles', :action => 'by_category'
+  map.root :controller => 'articles'
 end
